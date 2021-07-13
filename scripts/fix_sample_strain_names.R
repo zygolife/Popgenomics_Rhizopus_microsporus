3eede4d140480a97762ca7adc74157a615f473a7
Jason Stajich <jasonstajich.phd@gmail.com>
2021-07-12 21:51:19 -0700

    samples are in the lib folder

--- scripts/fix_sample_strain_names.R
+++ scripts/fix_sample_strain_names.R
@@ -1,7 +1,7 @@
 library(readr)
 library(tidyverse)
 samp <- read_csv("samples.csv")
-sra <- read_tsv("PRJNA526061_SRA.tab")
+sra <- read_tsv("lib/PRJNA526061_SRA.tab")
 colnames(samp) <- c("RUN","FILEBASE")
 comb <- full_join(samp,sra,by="RUN")
 write_csv(comb,"samp_update.csv")
