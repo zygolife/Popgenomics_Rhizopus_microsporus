e45ae57d7b3114ef7257ad154a86614f029ab42d
Jason Stajich <jasonstajich.phd@gmail.com>
2021-07-12 21:49:35 -0700

    project specific download

--- pipeline/00_index.sh
+++ pipeline/00_index.sh
@@ -7,24 +7,10 @@ fi
 mkdir -p $GENOMEFOLDER
 pushd $GENOMEFOLDER
 # THIS IS EXAMPLE CODE FOR HOW TO DOWNLOAD DIRECT FROM FUNGIDB
-RELEASE=39
-SPECIES=AfumigatusAf293
-URL=https://fungidb.org/common/downloads/release-${RELEASE}/$SPECIES
-PREF=FungiDB-${RELEASE}_${SPECIES}
-FASTAFILE=${PREF}_Genome.fasta
-DOMAINFILE=${PREF}_InterproDomains.txt
-GFF=${PREF}.gff
-## THIS IS FUNGIDB DOWNLOAD PART
-echo "working off $FASTAFILE - check if these don't match may need to update config/init script"
-
-if [ ! -f $DOMAINFILE ]; then
-	curl -O $URL/txt/$DOMAINFILE
-fi
-if [ ! -f $FASTAFILE ] ; then
-	curl -O $URL/fasta/data/$FASTAFILE
-fi
-if [ ! -f $GFF ]; then
-	curl -O $URL/gff/data/$GFF
+URL=https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/006/680/115/GCA_006680115.1_ASM668011v1/GCA_006680115.1_ASM668011v1_genomic.fna.gz
+FASTAFILE=$(basename $URL .fna.gz).fasta
+if  [ ! -s $FASTAFILE ]; then
+	curl $URL | pigz -dc > $FASTAFILE
 fi
 
 if [[ ! -f $FASTAFILE.fai || $FASTAFILE -nt $FASTAFILE.fai ]]; then
